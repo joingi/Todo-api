04af5a21930364d07477ce5c6d4ecc6f1e841ce9
Johann Ingi <joi.ingi@me.com>
2017-05-23 14:21:00 +0200

    Refactor code with underscore.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -22,14 +23,17 @@ app.get('/todos', function (req, res) {
 app.get('/todos/:id', function (req, res) {
     // parseInt converts id from string to number
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo;
+    // using underscore.js
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    // var matchedTodo;
 
     // Iterate todos array and find match
-    todos.forEach(function(todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-    });
+    // todos.forEach(function(todo) {
+    //     if (todoId === todo.id) {
+    //         matchedTodo = todo;
+    //     }
+    // });
 
     if (matchedTodo) {
         res.json(matchedTodo);
@@ -54,11 +58,17 @@ app.get('/todos/:id', function (req, res) {
 
 // Post /todos
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
 
-    // add id field
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
+
     body.id = todoNextId;
     todoNextId++;
+    // or body.id = todoNextId++;
 
     // push body into array
     todos.push(body);
