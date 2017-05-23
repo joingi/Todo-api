27d3335d853ab68a5eaba04eaa0d88992963af73
Johann Ingi <joi.ingi@me.com>
2017-05-23 23:08:28 +0200

    add PUT request to todo

--- server.js
+++ server.js
@@ -10,89 +10,83 @@ var todoNextId = 1;
 app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
-    res.send('Todo api Root');
+	res.send('Todo API Root');
 });
-
 
 // GET /todos
 app.get('/todos', function (req, res) {
-    res.json(todos);
+	res.json(todos);
 });
 
-// Get /todos/:id
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
-    // parseInt converts id from string to number
-    // reg.params.id gives access to url
-    var todoId = parseInt(req.params.id, 10);
-    // using underscore.js
-    var matchedTodo = _.findWhere(todos, {id: todoId});
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-    // var matchedTodo;
-
-    // Iterate todos array and find match
-    // todos.forEach(function(todo) {
-    //     if (todoId === todo.id) {
-    //         matchedTodo = todo;
-    //     }
-    // });
-
-    if (matchedTodo) {
-        res.json(matchedTodo);
-    } else {
-        res.status(404).send();
-    }
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
 });
 
-// ++++ OPTION USING find method ++++
-// app.get('/todos/:id', function (req, res) {
-//     var todoId = parseInt(req.params.id, 10);
-//     var matchedTodo = todos.find(function (todo) {
-//       return todo.id === todoId;
-//     });
-
-//     if (matchedTodo) {
-//       res.json(matchedTodo);
-//     } else {
-//       res.status(404).send();
-//     }
-// });
-
-// Post /todos
+// POST /todos
 app.post('/todos', function (req, res) {
-    var body = _.pick(req.body, 'description', 'completed');
+	var body = _.pick(req.body, 'description', 'completed');
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
 
-    body.description = body.description.trim();
+	body.description = body.description.trim();
+	body.id = todoNextId++;
 
-    body.id = todoNextId;
-    todoNextId++;
-    // or body.id = todoNextId++;
+	todos.push(body);
 
-    // push body into array
-    todos.push(body);
-
-    res.json(body);
+	res.json(body);
 });
 
 // DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
-    var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
+	if (!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"});
+	} else {
+		todos = _.without(todos, matchedTodo);
+		res.json(matchedTodo);
+	}
+});
 
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
 
-    if (!matchedTodo) {
-        res.status(404).json({"error": "No todo item found with that id"});
-    } else {
-        todos =_.without(todos, matchedTodo);
-        res.json(matchedTodo);
-    }
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
 
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+		validAttributes.completed = body.completed;
+	} else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	matchedTodo = _.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
-    console.log('Express listening on port ' + PORT + '!');
+	console.log('Express listening on port ' + PORT + '!');
 });
+
