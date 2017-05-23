8797645238094c0c38e8e59c3c173227a34fb4e6
Johann Ingi <joi.ingi@me.com>
2017-05-23 11:10:02 +0200

    Add POST request for todos

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: 'Sækja um skóla',
-    completed: false
-},{
-    id: 2,
-    description: 'Finna einkanir',
-    completed: false
-},{
-    id: 3,
-    description: 'byrja á Node.js',
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     res.send('Todo api Root');
@@ -27,6 +20,7 @@ app.get('/todos', function (req, res) {
 
 // Get /todos/:id
 app.get('/todos/:id', function (req, res) {
+    // parseInt converts id from string to number
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo;
 
@@ -43,6 +37,7 @@ app.get('/todos/:id', function (req, res) {
         res.status(404).send();
     }
 });
+
 // ++++ OPTION USING find method ++++
 // app.get('/todos/:id', function (req, res) {
 //     var todoId = parseInt(req.params.id, 10);
@@ -56,6 +51,20 @@ app.get('/todos/:id', function (req, res) {
 //       res.status(404).send();
 //     }
 // });
+
+// Post /todos
+app.post('/todos', function (req, res) {
+    var body = req.body;
+
+    // add id field
+    body.id = todoNextId;
+    todoNextId++;
+
+    // push body into array
+    todos.push(body);
+
+    res.json(body);
+});
 
 app.listen(PORT, function () {
     console.log('Express listening on port ' + PORT + '!');
