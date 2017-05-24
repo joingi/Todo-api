d8407657446bfd40261f987122e0b7585a641486
Johann Ingi <joi.ingi@me.com>
2017-05-24 11:33:55 +0200

    Added q= filter to search description

--- server.js
+++ server.js
@@ -13,17 +13,21 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos?completed=true // Search function
+// GET /todos?completed=true&q=house // Search function
 app.get('/todos', function (req, res) {
     var queryParams = req.query;
     var filteredTodos = todos;
 
-    // if has property && completed === 'true'
-    //  filteredTodos = _.Where(filteredTodos, ?)
     if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
         filteredTodos = _.where(filteredTodos, {completed: true} );
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
         filteredTodos = _.where(filteredTodos, {completed: false} );
+    }
+
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0 ) {
+        filteredTodos = _.filter(filteredTodos, function (todo) {
+            return todo.description.indexOf(queryParams.q) > -1;
+        });
     }
 
 	res.json(filteredTodos);
