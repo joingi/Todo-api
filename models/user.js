1eeeb92e3a4187c73d4a6167e6a2e9c63b2a1bea
Johann Ingi <joi.ingi@me.com>
2017-05-30 22:14:31 +0200

    Add login route and authenticate class method to user login

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ var bcrypt = require('bcrypt');
 var _ = require('underscore');
 
 module.exports = function (sequelize, DataTypes) {
-	return sequelize.define('user', {
-		email: {
+    var user = sequelize.define('user', {
+        email: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
@@ -35,22 +35,45 @@ module.exports = function (sequelize, DataTypes) {
                 this.setDataValue('password_hash', hashedPassword);
             }
         }
-    },{
+    }, {
         hooks: {
-			beforeValidate: function (user, options) {
-				// user.email
-				if (typeof user.email === 'string') {
-					user.email = user.email.toLowerCase();
-				}
-			}
-		},
+            beforeValidate: function (user, options) {
+                // user.email
+                if (typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase();
+                }
+            }
+        },
+        classMethods: {
+            authenticate: function (body) {
+                return new Promise(function (resolve, reject) {
+                    if (typeof body.email !== 'string' && typeof body.password !== 'string') {
+                        return reject();
+                    }
+
+                    user.findOne({
+                        where: {
+                            email: body.email
+                        }
+                    }).then(function (user) {
+                        if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+                            return reject();
+                        }
+                        resolve(user);
+                    }, function (e) {
+                        reject();
+                    });
+                });
+            }
+        },
         instanceMethods: {
             toPublicJSON: function () {
                 var json = this.toJSON();
-                return _.pick(json, 'id','email', 'createdAt', 'updatedAt');
+                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
             }
         }
-	});
+    });
+    return user;
 };
 
 
