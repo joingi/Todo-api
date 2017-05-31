45cb62d92c0d8704b928f98ae6a7cc89f44b7a87
Johann Ingi <joi.ingi@me.com>
2017-05-31 11:00:11 +0200

    Add Auth middleware and fundByToken class method

--- models/user.js
+++ models/user.js
@@ -47,57 +47,79 @@ module.exports = function (sequelize, DataTypes) {
             }
         },
         classMethods: {
-            authenticate: function (body) {
-                return new Promise(function (resolve, reject) {
-                    if (typeof body.email !== 'string' && typeof body.password !== 'string') {
-                        return reject();
-                    }
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
 
-                    user.findOne({
-                        where: {
-                            email: body.email
-                        }
-                    }).then(function (user) {
-                        if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-                            return reject();
-                        }
-                        resolve(user);
-                    }, function (e) {
-                        reject();
-                    });
-                });
-            }
-        },
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
+							return reject();
+						}
+
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
+				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, reject) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#!');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+						user.findById(tokenData.id).then(function (user) {
+							if (user) {
+								resolve(user);
+							} else {
+								reject();
+							}
+						}, function (e) {
+							reject();
+						});
+					} catch (e) {
+						reject();
+					}
+				});
+			}
+		},
         instanceMethods: {
-            toPublicJSON: function () {
-                var json = this.toJSON();
-                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
-            },
-            generateToken: function (type) {
-                if (!_.isString(type)) {
-                    return undefined;
-                }
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
 
-                try {
-                    // stringData connverts data= id and password to string
-                    var stringData = JSON.stringify({
-                        id: this.get('id'),
-                        type: type
-                    });
-                    var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123#!').toString();
-                    var token = jwt.sign({
-                        token: encryptedData,
-                    }, 'querty098');
+				try {
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwerty098');
 
-                    return token;
-                } catch (e) {
-                    console.error(e);
-                    return undefined;
-                }
-            }
-        }
-    });
-    return user;
+					return token;
+				} catch (e) {
+					console.error(e);
+					return undefined;
+				}
+			}
+		}
+	});
+
+	return user;
 };
 
 
